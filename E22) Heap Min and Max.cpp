fabe6ccb229eb146e331c8bc103cd393ad1f3de7
Farhan Ansari <162814429+Farhanansari07@users.noreply.github.com>
2025-05-08 21:12:58 +0530

    Update and rename 22.heap student marks.cpp to E22) Heap Min and Max.cpp

--- E22) Heap Min and Max.cpp
+++ E22) Heap Min and Max.cpp
@@ -1,3 +1,8 @@
+/*
+Read the marks obtained by students of second year in an online examination of particular subject. 
+Find out maximum and minimum marks obtained in that subject. Use heap data structure. Analyze the algorithm.
+*/
+
 #include <iostream>
 using namespace std;
 
